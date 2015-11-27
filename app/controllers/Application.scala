7b6ad34aab2bffa2075da04e3cbb976d97ba5007
jonghoon kim <infoset@gmail.com>
2015-11-27 00:55:56 -0500

    route / to index.html

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,7 +5,9 @@ import play.api.mvc._
 
 class Application extends Controller {
 
-  def index = Action {
+  def index = controllers.Assets.at("/public", "index.html")
+
+  def testApiList = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 }
