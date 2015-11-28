bf3a757f8497df8a5874853b696b9290ffefe4ed
jonghoon kim <infoset@gmail.com>
2015-11-27 22:34:37 -0500

    implement BackendService

--- public/app/module.js
+++ public/app/module.js
@@ -4,22 +4,47 @@ var cadsApp = angular.module('cadsApp', ['ui.router']);
 
 cadsApp.factory('BackendService', ['$http', function(http) {
     
-    var credential = {
-        id: "",
-        token: ""
-    };
+    var credential = { email: "", token: "" };
 
     return {
-        login: function(id, pw) {
+        getUserCredential: function() {
+            return credential;
         },
-        call: function(configObj, successCallback, errorCallback) {
+        isUserLoggedIn: function() {
+            return !(credential.email.length === 0);
+        },
+        login: function(email, pw, successCallback, errorCallback) {
+            http({ method: "POST", url: "/users/login", data: {email: email, password: pw} })
+            .then(
+                function(response) { // succeeded
+                    credential = response.data;
+                    http.defaults.headers.common.Authorization = credential.token;
+                    successCallback(response);
+                },
+                function(response) { // failed
+                    http.defaults.headers.common.Authorization = "";
+                    errorCallback(response);
+                });
+        },
+        callApi: function(configObj, successCallback, errorCallback) {
             
-            if (credential.id.length === 0) {
-                errorCallback({status:404, statusText: "unauthorized"});
+            if (credential.email.length === 0) {
+                errorCallback({status:401, statusText: "Unauthorized"});
                 return;
             }
 
-            http(configObj).then(successCallback, errorCallback);
+            http(configObj)
+            .then(
+                function(response) {
+                    successCallback(response);
+                },
+                function(response) {
+                    if (401 === response.status) { // unauthorized
+                        credential = {email: "", token: ""}; // clear credential
+                        $http.defaults.headers.common.Authorization = "";
+                    }
+                    errorCallback(response);
+                });
         }
     };
 }]);
@@ -28,14 +53,20 @@ cadsApp.controller('TestController', ['$scope','BackendService', function ($scop
     
     $scope.message = "";
 
+    $scope.login = function(email, password) {
+        bs.login(email, password, 
+            function(r) { $scope.message = r.statusText; },
+            function(r) { $scope.message = r.statusText; });
+    };
+
     $scope.click = function() {
-        bs.call(
+        bs.callApi(
             {method:'GET', url: '/users'}, 
             function(r) { 
                 console.log(r.data);
             }, 
             function(r) { 
-                $scope.message = r.statusText; 
+                $scope.message = r.statusText;
             });
     };
  }]);
