768ce72911f36c9a8d895e93638d10ea136a8460
bslee <hidcliff@gmail.com>
2015-11-30 15:28:37 -0500

    add test runner and framework

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
+var Server = require('karma').Server;
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
 });
