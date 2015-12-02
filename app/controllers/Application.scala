f789b448337b7c93be7f8a0ea6357e52a6d0686e
Jong Hoon Kim <john@Jongs-MacBook-Air.local>
2015-12-01 19:11:46 -0500

    return proper index.html based on application mode

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,7 +5,8 @@ import play.api.mvc._
 
 class Application extends Controller {
 
-  def index = controllers.Assets.at("/public", "index.html")
+  def index = controllers.Assets.at("/public", 
+  	(if (Play.current.mode == Mode.Dev) "index.html" else "dist/index.html"))
 
   def testApiList = Action {
     Ok(views.html.index("Your new application is ready."))
